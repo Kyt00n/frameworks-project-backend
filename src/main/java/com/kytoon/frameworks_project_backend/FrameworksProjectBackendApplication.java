e16784e50cadfd144c10cff231e84d717deeada4
Kytoon <eryk.olsza@gmail.com>
2024-11-28 01:47:18 +0100

    Initial commit

---
+++ src/main/java/com/kytoon/frameworks_project_backend/FrameworksProjectBackendApplication.java
@@ -0,0 +1,13 @@
+package com.kytoon.frameworks_project_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrameworksProjectBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrameworksProjectBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kytoon/frameworks_project_backend/FrameworksProjectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kytoon.frameworks_project_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrameworksProjectBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
